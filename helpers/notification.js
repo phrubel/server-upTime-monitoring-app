1f217bbfc9490b6ab3341fec0ca3d36cbd07dfea
phrubel <phrubel42@gmail.com>
2024-11-27 15:50:53 +0600

    notification system just start

---
+++ helpers/notification.js
@@ -0,0 +1,17 @@
+/*
+Comments:
+    * Title: Notification
+    * Description: handle all notifications via twilio.
+    * Author: Parvez Hasan Rubel
+    * Date: 2711/2024
+    * version: 1.0.0: 
+*/
+
+// dependencies
+const https = require('https');
+
+// module scaffolding
+const notification = {};
+
+// notification send
+notification.send = (phone, msg, callback) => {};
